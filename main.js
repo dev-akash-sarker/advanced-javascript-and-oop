36d1913de39eca424fad544a99b9334d8f1eb96f
Akash <45712184+dev-akash-sarker@users.noreply.github.com>
2025-12-01 16:00:44 +0600

    all

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-
-
-let a = 10; // global scope
-{ 
-    let b = 10;
-} // this is block scope
-
-function allin (){
-    let x = 20
-} // this is function scope
-function allinout (){
-    let x = 20
-} // this is function scope
-
-function outer () {
-    let b = 20;
-    function inner () {
-        let c = 30;
-        console.log(a, b , c)
+function outer(){
+    let counter  = 0;
+    console.log("outer")
+    return function inner(){
+        counter++;
+        console.log(counter);
     }
-    inner()
 }
-outer() // outer nested with inner so this is nested function scope
+
+const fn = outer();
+
+fn();
--- readme.md
+++ readme.md
@@ -1,17 +1,7 @@
 # Advanced Concept
 
-- Nested Function's scope
+## Closures
 
-There are 3 types of scope:
+- A closure is the combination of a function bundled together with references to its surrounding state. Clousures are created every time a function is created, at function creation time.
 
-## Block Scope
 
-- Variables declared insdie a pair of curly braces cannot be accessed from outside the block.
-
-## Function Scope
-
-- variables declared inside a function cannot not be accessed from outside the function
-
-## Global Scope
-
-- globally scoped variables cn be accessed inside a block or function
